30b06b842ddbd5ff74c32e707a616b62e1c9aa7d
LoukaG <66456642+LoukaG@users.noreply.github.com>
2022-02-24 15:45:39 -0500

    Scène

--- Scene.cpp
+++ Scene.cpp
@@ -1,11 +1,24 @@
 #include "Scene.h"
 
-Scene::Scene() {
 
-}
 
 void Scene::draw() {
+	for(int layer=9;layer>0;layer--)
+		for (GameObject& obj : gameObjets[layer]) {
+			//obj.render();
+		}
+		
+	
 }
 
 void Scene::update() {
+	for (int layer = 0; layer < 10; layer++)
+		for (GameObject& obj : gameObjets[layer]) {
+			//update
+		}
+		
+}
+
+void Scene::addGameObject(GameObject obj, unsigned int layer) {
+	gameObjets[layer].push_back(obj);
 }
--- Scene.h
+++ Scene.h
@@ -2,18 +2,21 @@
 #define SCENE_CLASS_H
 #include <iostream>
 #include <vector>
+#include "Camera.h"
 
 #include "GameObject.h"
 
 
 class Scene {
 private:
-	std::vector<GameObject> gameObjets;
+	std::vector<GameObject> gameObjets[10];
+	Camera camera;
 
 public:
 	Scene();
 	void draw();
 	void update();
+	void addGameObject(GameObject obj, unsigned int layer);
 };
 
 #endif
