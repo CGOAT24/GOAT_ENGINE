77be15755bb73d67eee01e2b160427f70bf02bca
Chad Gauthier <chad.gauthier.01@edu.cegeptr.qc.ca>
2022-03-02 14:17:59 -0500

    résolutions bugs

--- Camera.cpp
+++ Camera.cpp
@@ -1,16 +1,16 @@
 #include "Camera.h"
 
-Camera::Camera(int width, int height, glm::vec3 position) {
-	Camera::width = width;
-	Camera::height = height;
-	position = position;
+Camera::Camera(int _width, int _height, glm::vec3 _position) {
+	Camera::width = _width;
+	Camera::height = _height;
+	position = _position;
 }
 
 void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane) {
 	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 projection = glm::mat4(1.0f);
 
-	view = glm::lookAt(position, position + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	view = glm::lookAt(position, position + Orientation, Up);
 	projection = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
 
 	cameraMatrix = projection * view;
--- Camera.h
+++ Camera.h
@@ -11,13 +11,15 @@
 #include "shaderClass.h"
 
 class Camera {
-private:
+public:
+	glm::vec3 position;
+	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::mat4 cameraMatrix = glm::mat4(1.0f);
 	int width;
 	int height;
+	float speed = 0.001f;
 
-public:
-	glm::vec3 position;
 	Camera(int width, int height, glm::vec3 position);
 	void updateMatrix(float FOVdeg, float nearPlane, float farPlane);
 	void matrix(Shader& shader, const char* uniform);
