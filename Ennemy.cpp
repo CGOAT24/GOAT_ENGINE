7ca420b0d46116292dd478f9c33af3b9d66a55f0
Chad Gauthier <chad.gauthier.01@edu.cegeptr.qc.ca>
2022-03-28 11:12:26 -0400

    updateTexture ennemy

--- Ennemy.cpp
+++ Ennemy.cpp
@@ -1,6 +1,6 @@
 #include "Ennemy.h"
 
-Ennemy::Ennemy(glm::vec2 position, char _color, GOAT_ENGINE::Texture _textures[4]): GameObject(position, glm::vec2(1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), (std::filesystem::current_path().string() + "\\textures\\default.png").c_str(), true), direction(0), canBeEaten(false), color(_color) {
+Ennemy::Ennemy(glm::vec2 position, GOAT_ENGINE::Texture _textures[4]): GameObject(position, glm::vec2(1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), (std::filesystem::current_path().string() + "\\textures\\default.png").c_str(), true), direction(0), canBeEaten(false) {
 	for (int i(0); i < 4; i++) {
 		this->textures[i] = _textures[i];
 	}
@@ -17,7 +17,7 @@ void Ennemy::updateCanBeEaten(bool eaten) {
 		this->mesh.texture = GOAT_ENGINE::Texture((std::filesystem::current_path().string() + "\\textures\\ennemies\\dead.png").c_str());
 	}
 	else {
-		this->mesh.texture = GOAT_ENGINE::Texture((this->getTexPath()).c_str());
+		this->mesh.texture = this->getTexture();
 	}
 }
 
@@ -44,40 +44,6 @@ void Ennemy::move() {
 	}
 }
 
-//à updater
-std::string Ennemy::getTexPath() {
-	const std::string path((std::filesystem::current_path().string() + "\\textures\\ennemies\\").c_str());
-	std::string texName = "";
-
-	switch (color) {
-	case 'c':
-		texName += "cyan_";
-		break;
-	case 'o':
-		texName += "orange_";
-		break;
-	case 'p':
-		texName += "pink_";
-		break;
-	case 'r':
-		texName += "red_";
-		break;
-	}
-
-	switch (direction) {
-	case 0:
-		texName += "up.png";
-		break;
-	case 1:
-		texName += "down.png";
-		break;
-	case 2:
-		texName += "left.png";
-		break;
-	case 3:
-		texName += "right.png";
-		break;
-	}
-
-	return path + texName;
+GOAT_ENGINE::Texture Ennemy::getTexture() {
+	return this->textures[direction];
 }
--- Ennemy.h
+++ Ennemy.h
@@ -8,12 +8,11 @@ private:
 	GOAT_ENGINE::Texture textures[4];
 	int direction;
 	bool canBeEaten;
-	char color;
 
-	std::string getTexPath();
+	GOAT_ENGINE::Texture getTexture();
 
 public:
-	Ennemy(glm::vec2 position, char color, GOAT_ENGINE::Texture _textures[4]);
+	Ennemy(glm::vec2 position, GOAT_ENGINE::Texture _textures[4]);
 	char getDirection() const;
 	void updateCanBeEaten(bool eaten);
 	void move();
